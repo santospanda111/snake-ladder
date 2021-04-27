306af0bf0f3b6afc77f7bfec64a25f9685565632
santospanda111 <santospanda111@gmail.com>
2021-04-27 23:22:03 +0530

    UC4 code added

--- snakeladder/src/snakeladder/snakeladder.java
+++ snakeladder/src/snakeladder/snakeladder.java
@@ -1,26 +1,36 @@
 package snakeladder;
 
 public class snakeladder {
-
+	public static final int source = 0;
+	public static final int destination = 100;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int position=0;
-		int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
-		int randomPlay = (int) (Math.random() * 10) % 3;
-		switch(randomPlay) {
-		case 0:
-			System.out.println("No play");
-			break;
-		case 1:
-			System.out.println("Ladder");
-			position += randomCheck;
-			break;
-		case 2:
-			System.out.println("Snake");
-			position -= randomCheck;
-			break;
-		}
+		int position=0,diceRoll=0;
+		while(position != destination)
+		{
+			diceRoll++;
+				int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+				int randomPlay = (int) (Math.random() * 10) % 3;
+				switch(randomPlay) {
+				case 0:
+					System.out.println("No play");
+					break;
+				case 1:
+					System.out.println("Well done");
+					position += randomCheck;
+					break;
+				case 2:
+					System.out.println("Snake");
+					position -= randomCheck;
+					break;
+				}
+				if(position < 0) {
+					position = 0;}
+				else if(position>=100) {
+					position=100;}
+		  }
+		System.out.println(position);
+	  }
+  }
 
-	}
 
-}
