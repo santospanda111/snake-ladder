eca1dd0f44655b80bb2f3cce204313359c222670
santospanda111 <santospanda111@gmail.com>
2021-04-27 22:47:28 +0530

    added UC3 code

--- snakeladder/src/snakeladder/snakeladder.java
+++ snakeladder/src/snakeladder/snakeladder.java
@@ -6,6 +6,20 @@ public class snakeladder {
 		// TODO Auto-generated method stub
 		int position=0;
 		int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+		int randomPlay = (int) (Math.random() * 10) % 3;
+		switch(randomPlay) {
+		case 0:
+			System.out.println("No play");
+			break;
+		case 1:
+			System.out.println("Ladder");
+			position += randomCheck;
+			break;
+		case 2:
+			System.out.println("Snake");
+			position -= randomCheck;
+			break;
+		}
 
 	}
 
