b1f6dbe76d4fc503704e1535d5e180bea1c7487e
santospanda111 <santospanda111@gmail.com>
2021-04-27 22:38:40 +0530

    added UC2 code

--- snakeladder/src/snakeladder/snakeladder.java
+++ snakeladder/src/snakeladder/snakeladder.java
@@ -5,6 +5,7 @@ public class snakeladder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int position=0;
+		int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
 
 	}
 
